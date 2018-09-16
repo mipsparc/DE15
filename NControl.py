8245f8f15dc5524a88c9e226d8266e8f0d2832d6
mipsparc <mipsparc@gmail.com>
2018-09-16 15:56:32 +0900

    uncommented the mascon feature

--- NControl.py
+++ NControl.py
@@ -8,15 +8,15 @@ from multiprocessing import Process, Value
 import time
 
 ## マスコン読み込みプロセス起動
-#def ReadMasconWorker(mascon_shared, device):    
-    #mascon = MasconReader.ReadMascon(device)
-    #while True:
-        #mascon_level = mascon.waitAndGetMascon()
-        #mascon_shared.value = mascon_level
+def ReadMasconWorker(mascon_shared, device):    
+    mascon = MasconReader.ReadMascon(device)
+    while True:
+        mascon_level = mascon.waitAndGetMascon()
+        mascon_shared.value = mascon_level
 
-#mascon_shared = Value('i', 0)
-#mascon_process = Process(target=ReadMasconWorker, args=(mascon_shared, '/dev/ttyUSB0'))
-#mascon_process.start()
+mascon_shared = Value('i', 0)
+mascon_process = Process(target=ReadMasconWorker, args=(mascon_shared, '/dev/ttyUSB0'))
+mascon_process.start()
 
 # ブレーキ読み込みプロセス起動
 def ReadBrakeWorker(brake_shared, device):    
@@ -26,22 +26,22 @@ def ReadBrakeWorker(brake_shared, device):
         brake_shared.value = brake_level
 
 brake_shared = Value('f', 0.0)
-brake_process = Process(target=ReadBrakeWorker, args=(brake_shared, '/dev/ttyUSB0'))
-#brake_process = Process(target=ReadBrakeWorker, args=(brake_shared, '/dev/ttyUSB2'))
+brake_process = Process(target=ReadBrakeWorker, args=(brake_shared, '/dev/ttyUSB2'))
 brake_process.start()
 
 
 DE101 = DE10.DE10()
-#controller = Controller.Controller('/dev/ttyUSB1')
+controller = Controller.Controller('/dev/ttyUSB1')
 
-# 精密カウンターでメインループを0.1秒おきに回す
+# メインループを0.1秒おきに回す
 last_counter = time.time()
 
 while True:
-    #mascon_level = mascon_shared.value
+    mascon_level = mascon_shared.value
+    mascon_level = 14
     brake_level = brake_shared.value
     
-    #DE101.setMascon(mascon_level)
+    DE101.setMascon(mascon_level)
     DE101.setBrake(brake_level)
     
     # 0.1秒経過するまでwaitする
@@ -50,7 +50,7 @@ while True:
     last_counter = time.time()
     
     DE101.advanceTime()
-    print(DE101.getBp())
-    #speed = DE101.getSpeed()
+    speed = DE101.getSpeed()
+    print(speed * 3600 / 1000)
     
-    #controller.move(speed)
+    controller.move(speed)
