72fd47b48ab1d6a4d3e4dfb38463a50891dcec8e
mipsparc <mipsparc@gmail.com>
2018-09-16 16:03:01 +0900

    fixed namespace bug

--- Controller.py
+++ Controller.py
@@ -4,10 +4,6 @@ import serial
 
 class Controller:
     def __init__(self, device):
-        #出力値の傾き
-        self.magnify_1 = 1.0
-        self.magnify_2 = 0.5
-        
         # 2回に一回書き込む
         self.write_count = False
         self.device = serial.Serial(device, baudrate=9600)
@@ -17,11 +13,18 @@ class Controller:
         # 動き始める速度
         base_power = 42
         
+        #出力値の傾き
+        magnify_1 = 1.0
+        magnify_2 = 0.5
+        
+        #傾きが変化する閾値
+        criteria = 5.5
+        
         #1次関数を2つ繋げる
         if speed < 5.5:
             output_power = (speed * magnify_1) + base_power
         else:
-            output_power = (speed * magnify_2) + (5.5 * magnify_1) + base_power
+            output_power = (speed * magnify_2) + (criteria * magnify_1) + base_power
             
         if output_power > 255:
             output_power = 255
